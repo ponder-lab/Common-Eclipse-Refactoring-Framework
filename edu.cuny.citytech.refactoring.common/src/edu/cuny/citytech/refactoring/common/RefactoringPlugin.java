62942c59c171e886e13758b124f33232ec8b2cdc
Raffi Khatchadourian <rkhatchadourian@citytech.cuny.edu>
2015-06-10 19:52:07 +0900

    I believe that singleton instances need to be in subclasses (no static "inheritance").

--- edu.cuny.citytech.refactoring.common/src/edu/cuny/citytech/refactoring/common/RefactoringPlugin.java
+++ edu.cuny.citytech.refactoring.common/src/edu/cuny/citytech/refactoring/common/RefactoringPlugin.java
@@ -6,32 +6,28 @@ package edu.cuny.citytech.refactoring.common;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
-import org.osgi.framework.BundleContext;
 
 /**
+ * Base class for all refactoring plugins. 
+ * 
  * @author <a href="mailto:rkhatchadourian@citytech.cuny.edu">Raffi
  *         Khatchadourian</a>
- *
  */
 public abstract class RefactoringPlugin extends AbstractUIPlugin {
 
-	protected static RefactoringPlugin plugin;
-
-	public static RefactoringPlugin getDefault() {
-		return plugin;
-	}
-
+	/**
+	 * Returns this refactoring's identifier.
+	 * @return This Refactori's identifier.
+	 */
 	protected abstract String getRefactoringId();
 
+	/**
+	 * Log a Throwable to the log for this plugin as an error.
+	 * @param throwable The Throwable to log.
+	 */
 	public void log(Throwable throwable) {
-		getDefault().getLog().log(
+		this.getLog().log(
 				new Status(IStatus.ERROR, this.getRefactoringId(), 0, throwable
 						.getMessage(), throwable));
 	}
-
-	public void stop(BundleContext context) throws Exception {
-		plugin = null;
-		super.stop(context);
-	}
-
 }
