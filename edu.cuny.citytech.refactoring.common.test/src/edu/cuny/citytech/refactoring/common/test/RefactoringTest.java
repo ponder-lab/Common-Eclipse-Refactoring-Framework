2838b9c7e43ac839ffd6b056ed4d27e5f43821ca
WaLiD <arefin.shahrear@gmail.com>
2015-08-03 17:19:11 -0400

    adding constructor

---
+++ edu.cuny.citytech.refactoring.common.test/src/edu/cuny/citytech/refactoring/common/test/RefactoringTest.java
@@ -0,0 +1,11 @@
+package edu.cuny.citytech.refactoring.common.test;
+
+@SuppressWarnings("restriction")
+public class RefactoringTest extends org.eclipse.jdt.ui.tests.refactoring.RefactoringTest {
+
+	public RefactoringTest(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+}
