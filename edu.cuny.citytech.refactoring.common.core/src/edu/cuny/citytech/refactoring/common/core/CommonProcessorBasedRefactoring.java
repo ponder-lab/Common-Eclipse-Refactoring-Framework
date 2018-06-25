d90427185f97ba4cc45f7716915bc34d246c719f
Yiming <ytang3@gradcenter.cuny.edu>
2018-06-25 18:36:36 -0400

    rename package

--- edu.cuny.citytech.refactoring.common.core/src/edu/cuny/citytech/refactoring/common/core/CommonProcessorBasedRefactoring.java
+++ edu.cuny.citytech.refactoring.common.core/src/edu/cuny/citytech/refactoring/common/core/CommonProcessorBasedRefactoring.java
@@ -1,6 +1,8 @@
-package edu.cuny.citytech.refactoring.common.core.refactorings;
+package edu.cuny.citytech.refactoring.common.core;
 
 import org.eclipse.ltk.core.refactoring.participants.ProcessorBasedRefactoring;
+
+import edu.cuny.citytech.refactoring.common.core.refactorings.CommonRefactoringProcessor;
 
 public class CommonProcessorBasedRefactoring extends ProcessorBasedRefactoring{
 
