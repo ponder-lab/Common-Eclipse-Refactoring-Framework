6f841527de9d4e680742092cdc0d76bb1b4beaa6
Yiming <ytang3@gradcenter.cuny.edu>
2018-06-25 23:20:01 -0400

    rename processor

--- edu.cuny.citytech.refactoring.common.core/src/edu/cuny/citytech/refactoring/common/core/ProcessorBasedRefactoring.java
+++ edu.cuny.citytech.refactoring.common.core/src/edu/cuny/citytech/refactoring/common/core/ProcessorBasedRefactoring.java
@@ -1,10 +1,10 @@
 package edu.cuny.citytech.refactoring.common.core;
 
-import edu.cuny.citytech.refactoring.common.core.refactorings.CommonRefactoringProcessor;
+import edu.cuny.citytech.refactoring.common.core.refactorings.RefactoringProcessor;
 
 public class ProcessorBasedRefactoring extends org.eclipse.ltk.core.refactoring.participants.ProcessorBasedRefactoring {
 
-	public ProcessorBasedRefactoring(CommonRefactoringProcessor processor) {
+	public ProcessorBasedRefactoring(RefactoringProcessor processor) {
 		super.setProcessor(processor);
 	}
 
