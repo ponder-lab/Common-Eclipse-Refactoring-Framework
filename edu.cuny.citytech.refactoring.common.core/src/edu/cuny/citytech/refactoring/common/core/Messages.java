e13b53ffa84ea5056967ce6b43658199244612f0
Raffi Khatchaodourian <raffi.khatchadourian@hunter.cuny.edu>
2022-07-12 11:41:44 -0400

    Add messages.
    In case we need it later.

---
+++ edu.cuny.citytech.refactoring.common.core/src/edu/cuny/citytech/refactoring/common/core/Messages.java
@@ -0,0 +1,28 @@
+/**
+ *
+ */
+package edu.cuny.citytech.refactoring.common.core;
+
+import org.eclipse.osgi.util.NLS;
+
+/**
+ * @author <a href="mailto:raffi.khatchadourian@hunter.cuny.edu">Raffi
+ *         Khatchadourian</a>
+ */
+public class Messages extends NLS {
+	private static final String BUNDLE_NAME = "edu.cuny.citytech.refactoring.core.Messages"; //$NON-NLS-1$
+	public static String CheckingPreconditions;
+	public static String CompilingSource;
+	public static String CreatingChange;
+	public static String CUContainsCompileErrors;
+	public static String RefactoringNotPossible;
+
+	static {
+		// initialize resource bundle
+		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+	}
+
+	private Messages() {
+		// Do not instantiate.
+	}
+}
