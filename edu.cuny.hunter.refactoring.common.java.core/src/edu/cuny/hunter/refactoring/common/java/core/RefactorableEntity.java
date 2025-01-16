90993655c42a74ceb14a88103760fe6b7dbb9b7c
Raffi Khatchadourian <khatchad@hunter.cuny.edu>
2025-01-16 17:40:04 -0500

    Progress.

--- edu.cuny.hunter.refactoring.common.java.core/src/edu/cuny/hunter/refactoring/common/java/core/RefactorableEntity.java
+++ edu.cuny.hunter.refactoring.common.java.core/src/edu/cuny/hunter/refactoring/common/java/core/RefactorableEntity.java
@@ -1,15 +1,22 @@
 package edu.cuny.hunter.refactoring.common.java.core;
 
+import static org.eclipse.jdt.internal.corext.dom.ASTNodes.getParent;
+
 import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.TypeDeclaration;
 import org.eclipse.jdt.internal.corext.refactoring.structure.CompilationUnitRewrite;
 
 public abstract class RefactorableEntity extends edu.cuny.citytech.refactoring.common.core.RefactorableEntity {
 
 	public abstract ICompilationUnit getCompilationUnit();
 
-	public abstract CompilationUnit getEnclosingCompilationUnit();
+	public CompilationUnit getEnclosingCompilationUnit() {
+		return (CompilationUnit) getParent(this.getEnclosingTypeDeclaration(), ASTNode.COMPILATION_UNIT);
+	}
 
-	@SuppressWarnings("restriction")
+	public abstract TypeDeclaration getEnclosingTypeDeclaration();
+
 	public abstract void transform(CompilationUnitRewrite rewrite);
 }
