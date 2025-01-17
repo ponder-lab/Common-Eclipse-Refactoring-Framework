5fbac95fd9188c32dcdde61616929b37532b62a6
Raffi Khatchadourian <khatchad@hunter.cuny.edu>
2025-01-17 12:57:25 -0500

    Progress.

--- edu.cuny.hunter.refactoring.common.java.core/src/edu/cuny/hunter/refactoring/common/java/core/RefactoringProcessor.java
+++ edu.cuny.hunter.refactoring.common.java.core/src/edu/cuny/hunter/refactoring/common/java/core/RefactoringProcessor.java
@@ -1,7 +1,10 @@
 package edu.cuny.hunter.refactoring.common.java.core;
 
+import static edu.cuny.citytech.refactoring.common.core.Messages.CheckingPreconditions;
 import static edu.cuny.citytech.refactoring.common.core.Messages.NoElementsToRefactor;
+import static java.util.stream.Collectors.toSet;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -23,6 +26,8 @@ import org.eclipse.jdt.internal.corext.refactoring.util.RefactoringASTParser;
 import org.eclipse.jdt.internal.corext.refactoring.util.TextEditBasedChangeManager;
 import org.eclipse.ltk.core.refactoring.Change;
 import org.eclipse.ltk.core.refactoring.NullChange;
+import org.eclipse.ltk.core.refactoring.RefactoringStatus;
+import org.eclipse.ltk.core.refactoring.participants.CheckConditionsContext;
 
 import edu.cuny.citytech.refactoring.common.core.Messages;
 
@@ -31,7 +36,6 @@ import edu.cuny.citytech.refactoring.common.core.Messages;
  *
  * @author <a href="mailto:khatchad@hunter.cuny.edu">Raffi Khatchadourian</a>
  */
-@SuppressWarnings("restriction")
 public abstract class RefactoringProcessor extends edu.cuny.citytech.refactoring.common.core.RefactoringProcessor {
 
 	protected Set<RefactorableEntity> entities = new HashSet<>();
@@ -93,11 +97,41 @@ public abstract class RefactoringProcessor extends edu.cuny.citytech.refactoring
 	}
 
 	@Override
+	public RefactoringStatus checkFinalConditions(IProgressMonitor monitor, CheckConditionsContext context)
+			throws CoreException, OperationCanceledException {
+		RefactoringStatus status = new RefactoringStatus();
+		SubMonitor progress = SubMonitor.convert(monitor, CheckingPreconditions, 1);
+
+		// get the status of each stream.
+		RefactoringStatus collectedStatus = this.getEntities().parallelStream().map(RefactorableEntity::getStatus)
+				.collect(() -> new RefactoringStatus(), (a, b) -> a.merge(b), (a, b) -> a.merge(b));
+		status.merge(collectedStatus);
+		progress.worked(1);
+
+		// if there are no fatal errors.
+		if (!status.hasFatalError()) {
+			// these are the entities passing preconditions.
+			Set<RefactorableEntity> passingEntities = this.getPassingEntities();
+
+			// add a fatal error if there are no passing entities.
+			if (passingEntities.isEmpty())
+				status.addFatalError(Messages.NoElementsToRefactor);
+			else {
+				// TODO:
+				// Checks.addModifiedFilesToChecker(ResourceUtil.getFiles(fChangeManager.getAllCompilationUnits()),
+				// context);
+			}
+		}
+
+		return status;
+	}
+
+	@Override
 	public Change createChange(IProgressMonitor monitor) throws CoreException, OperationCanceledException {
 		try {
 			SubMonitor progress = SubMonitor.convert(monitor, Messages.CreatingChange, 100);
 
-			Set<RefactorableEntity> entities = this.getEntities();
+			Set<RefactorableEntity> entities = this.getPassingEntities();
 
 			if (entities.isEmpty())
 				return new NullChange(NoElementsToRefactor);
@@ -107,9 +141,7 @@ public abstract class RefactoringProcessor extends edu.cuny.citytech.refactoring
 			for (RefactorableEntity entity : entities) {
 				CompilationUnitRewrite rewrite = this.getCompilationUnitRewrite(entity.getCompilationUnit(),
 						entity.getEnclosingCompilationUnit());
-
 				entity.transform(rewrite);
-
 				transformProgress.worked(1);
 			}
 
@@ -120,7 +152,6 @@ public abstract class RefactoringProcessor extends edu.cuny.citytech.refactoring
 					.filter(cu -> !manager.containsChangesIn(cu)).toArray(ICompilationUnit[]::new);
 
 			SubMonitor saveProgress = progress.split(30).setWorkRemaining(units.length * 2);
-
 			for (ICompilationUnit cu : units) {
 				CompilationUnit compilationUnit = this.getCompilationUnit(cu, saveProgress.split(1));
 				manageCompilationUnit(manager, this.getCompilationUnitRewrite(cu, compilationUnit), saveProgress.split(1));
@@ -141,5 +172,20 @@ public abstract class RefactoringProcessor extends edu.cuny.citytech.refactoring
 
 	protected Set<RefactorableEntity> getEntities() {
 		return entities;
+	}
+
+	/**
+	 * @return True iff the {@link RefactorableEntity} {@link Set} changed as a result of this call.
+	 * @see java.util.Set#addAll
+	 */
+	protected boolean addEntities(Set<? extends RefactorableEntity> entities) {
+		return this.getEntities().addAll(entities);
+	}
+
+	public Set<RefactorableEntity> getPassingEntities() {
+		Set<RefactorableEntity> entities = this.getEntities();
+
+		return entities == null ? Collections.emptySet()
+				: entities.parallelStream().filter(s -> !s.getStatus().hasError()).collect(toSet());
 	}
 }
